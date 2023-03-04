bfbe2adb91d3466a8af5650965653b5f6a6afb45
Yasinovskiy Egor <yasinovskiy.egor1@gmail.com>
2023-03-04 06:15:57 +0300

    Остаточные

---
+++ BeautySalonBot/Service.cs
@@ -0,0 +1,23 @@
+namespace BeautySalonBot.Model
+{
+    public class Service
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public TimeSpan Duration { get; set; }
+        public Service()
+        {
+        }
+
+        public Service(Guid id, string name, string description, string category, TimeSpan duration)
+        {
+            Id = id;
+            Name = string.IsNullOrEmpty(name) ? throw new ArgumentNullException(nameof(name)) : name;
+            Description = string.IsNullOrEmpty(description) ? throw new ArgumentNullException(nameof(description)) : description;
+            Category = string.IsNullOrEmpty(category) ? throw new ArgumentNullException(nameof(category)) : category;
+            Duration = duration;
+        }
+    }
+}
