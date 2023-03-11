dceabad04c56c868281b799085544f8137211615
Yasinovskiy Egor <yasinovskiy.egor1@gmail.com>
2023-03-12 02:59:28 +0300

    Добавил ссылку на услугу, время записи и признак отмены записи

--- BeautySalonBot/Model/ServiceRecord.cs
+++ BeautySalonBot/Model/ServiceRecord.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BeautySalonBot.Domain.Model
+﻿namespace BeautySalonBot.Domain.Model
 {
-    public class ServiceRecord
-    {
-        public Guid Id { get; set; }
-        public Guid SpecialistId { get; set; }
-        public virtual Specialist Specialist { get; set; }
-        public Guid ClientId { get; set; }
-        public virtual Client Client { get; set; }
-    }
+	public class ServiceRecord
+	{
+		public Guid Id { get; set; }
+		public Guid SpecialistId { get; set; }
+		public virtual Specialist Specialist { get; set; }
+		public Guid ClientId { get; set; }
+		public virtual Client Client { get; set; }
+		public Guid ServiceId { get; set; }
+		public virtual Service Service { get; set; }
+		public DateTime RecordTime { get; set; }
+		public bool Cancelled { get; set; }
+	}
 }
